0c591b7f99a721ee970c0135beb266e743998f47
kjottkake <38797885+kjottkake@users.noreply.github.com>
2019-11-05 15:53:13 +0100

    Create C_Function_PrintMenu.c

---
+++ C_Function_PrintMenu.c
@@ -0,0 +1,11 @@
+void printMenu(void);
+
+void printMenu()  {
+    printf("\nList of Option:\n");
+    printf("\tF = Pays Respects\n");
+    printf("\tB = Build\n");
+    printf("\tR = Run\n");
+    printf("\tD = Dees nuts\n");
+    printf("\tY = Any key\n");
+    printf("\tQ = Quit\n");
+}

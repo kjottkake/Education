a9e40989e23258dce55e6d476d5849f3ee0eb344
kjottkake <38797885+kjottkake@users.noreply.github.com>
2019-11-21 16:46:41 +0100

    Create C_Switch.c

---
+++ C_Switch.c
@@ -0,0 +1,6 @@
+switch (variable)  {
+       case  'a':  something;   break;     
+       case  'b':  something;   break;
+       case  'c':  something;   break;
+       default:    something;   break;
+    }
